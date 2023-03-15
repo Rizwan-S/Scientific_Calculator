a52918acefa0351dea62c107c702e8ef8debf5ae
Mohd. Rizwan Shaikh <mohdrizwan.shaikh2112@gmail.com>
2023-03-15 19:56:16 +0530

    Implemented logging

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,4 +1,9 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Calculator {
+
+    private static final Logger logger = LogManager.getLogger(Calculator.class);
     public static double SquareRoot(double number) {
         if(number < 0) {
             System.out.println("Invalid Input: Negative Number ");
@@ -12,6 +17,8 @@ public class Calculator {
             System.out.println("SquareRoot function error");
             return -1;
         }
+
+        logger.info("RESULT - SquareRoot of " + number + " = " + answer);
         return answer;
     }
 
@@ -25,6 +32,8 @@ public class Calculator {
         for(int i = 1; i <= number; ++i) {
             answer = answer * i;
         }
+
+        logger.info("RESULT - Factorial of " + number + " = " + answer);
         return answer;
     }
 
@@ -41,11 +50,14 @@ public class Calculator {
             System.out.println("Natural log error: "+ e);
         }
 
+        logger.info("RESULT - NaturalLog of " + number + " = " + answer);
         return answer;
     }
 
     public static double Power(double base, double power) {
         double answer = (double)Math.pow(base, power);
+
+        logger.info("RESULT - Power of " + base + "^" + power +  " = " + answer);
         return answer;
     }
 }
